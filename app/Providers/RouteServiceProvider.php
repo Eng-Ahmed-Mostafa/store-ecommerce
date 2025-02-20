9543dde0233b1b41f79b8097ad9be84bff151102
Eng-Ahmed-Mostafa <ahmed.hash.developer@gmail.com>
2025-02-20 16:54:16 +0200

    finish

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,14 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('admin')
+                ->group(base_path('routes/admin.php'));
+
+            Route::middleware('site')
+                ->group(base_path('routes/site.php'));
         });
     }
+
+
 }
